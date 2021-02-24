3b0ac52c3cbb06da240350ca338bab6310f85517
armag3ddon <armag3ddon@web.de>
2021-02-24 02:22:47 +0100

    About command added.

---
+++ commands/about.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'über',
+	aliases: ['about', 'was'],
+	description: 'Infos über diesen Bot.',
+	execute(message, args) {
+		const data = [];
+
+		data.push('Hallo, ich bin der Potbot!');
+		data.push('\nIch wurde von Armag3ddon hergestellt. Fragen also dahin!');
+		data.push('\nWenn du wissen willst, wie ich funktioniere, dann geht das hier: https://github.com/Armag3ddon/potbot');
+
+		message.channel.send(data, { split: true });
+	},
+};
