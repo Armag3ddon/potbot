453a9e05571aadb0b63be46c410382b72d9b6a73
armag3ddon <armag3ddon@web.de>
2021-02-26 10:54:07 +0100

    Add !servers command to keep track of the bot's servers.

---
+++ commands/servers.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'servers',
+	description: '',
+	hidden: true,
+	execute(message) {
+		if (message.author.id != process.env.OWNER_ID) return;
+
+		const guilds = [];
+		guilds.push('Server:');
+		guilds.push(message.client.guilds.cache.map(guild => guild.name).join(', '));
+
+		message.channel.send(guilds);
+	},
+};
