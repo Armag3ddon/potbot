f12ca4c70c8b9cb43e8665c711fa573048f46d19
armag3ddon <armag3ddon@web.de>
2021-03-24 16:47:52 +0100

    Add lewd command.

---
+++ commands/lewd.js
@@ -0,0 +1,18 @@
+const { MessageAttachment } = require("discord.js");
+
+module.exports = {
+	name: 'lewd',
+	aliases: ['sperm', 'cum', 'orgasm'],
+	description: 'XXX',
+	filename: 'lewd',
+	execute(message) {
+		if (!message.channel.nsfw) return message.react('❌');
+
+		const pics = ['https://i.imgur.com/tsiOJbV.jpg', 'https://i.imgur.com/dRDXbC1.jpg', 'https://i.imgur.com/1HmwJyF.jpg', 'https://i.imgur.com/Auf1WbG.jpg'];
+		const rnd_pic = pics[Math.floor(Math.random() * pics.length)]
+
+		const attachment = new MessageAttachment(rnd_pic);
+
+		message.channel.send(attachment).then(msg => msg.react('💦'));
+	},
+};
