7d146d36daa8c051cbc59ebcc12eda54aa23eaf2
armag3ddon <armag3ddon@web.de>
2021-03-24 17:28:40 +0100

    Add boob command.

---
+++ commands/boob.js
@@ -0,0 +1,14 @@
+module.exports = {
+	name: 'titten',
+	aliases: ['eumel', 'boobs', 'moepse', 'boob', 'bazonkers'],
+	description: 'XXX',
+	filename: 'boob',
+	execute(message) {
+		if (!message.channel.nsfw) return message.react('❌');
+
+		const vids = ['https://i.imgur.com/jCuvFKP.mp4', 'https://i.imgur.com/taPc8YZ.mp4', 'https://i.imgur.com/hDUYPTB.mp4', 'https://i.imgur.com/PYmAXo8.mp4'];
+		const rnd_vid = vids[Math.floor(Math.random() * vids.length)]
+
+		message.channel.send(rnd_vid).then(msg => msg.react('💦'));
+	},
+};
