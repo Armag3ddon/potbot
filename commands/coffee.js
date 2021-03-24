456b83e9402ad1340327372d1e4f811cb4e2669d
armag3ddon <armag3ddon@web.de>
2021-03-24 16:09:55 +0100

    Add coffee command.

---
+++ commands/coffee.js
@@ -0,0 +1,11 @@
+module.exports = {
+	name: 'kaffee',
+	aliases: ['coffee', 'schwarzesgold'],
+	description: 'Ein Tässchen gefällig?',
+	filename: 'coffee',
+	execute(message, args) {
+		const coffee = ['Americano', 'Filterkaffee', 'Espresso', 'Cappucino', 'Latte Macchiato', 'Mokka', 'Cortado', 'Espresso Macchiato', 'Flat White', 'Caffè Latte'];
+		message.channel.send(':coffee:');
+		message.channel.send('Ich trinke jetzt einen ' + coffee[Math.floor(Math.random() * coffee.length)] + '.');
+	},
+};
