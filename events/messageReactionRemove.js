a2f0f9500f3b25f69fb456f36c24cfb13f744a35
armag3ddon <armag3ddon@web.de>
2021-04-02 16:44:26 +0200

    Update points on removal of reaction.

---
+++ events/messageReactionRemove.js
@@ -0,0 +1,34 @@
+const { Users, Messages } = require('../dbObjects.js');
+
+module.exports = {
+	name: 'messageReactionRemove',
+	async execute(messageReaction, user) {
+		if (user.bot) return;
+		// Disregard self reactions
+		// if (messageReaction.message.author.id == user.id) return;
+
+		if (!messageReaction.message.guild) return;
+		if (messageReaction.message.guild != process.env.CHGW_ID) return;
+
+		if (messageReaction.emoji.name == '🍺') {
+			const reaction_id = await Messages.getReactionId(messageReaction.message.id);
+			if (!reaction_id || reaction_id == '') {
+				// A rare occurences: a new reaction has arrived and the reaction message was not yet sent: do nothing in reaction message
+				return Users.addPoints(messageReaction.message.author.id, -1);
+			}
+			try {
+				messageReaction.message.channel.messages.fetch({ around: reaction_id, limit: 1 })
+					.then(messages => {
+						let points = messages.first().toString().match(/\((.*)\)/)[1];
+						points = parseInt(points);
+						points -= 1;
+						messages.first().edit('Katsching! (' + points + ')');
+					});
+			}
+			catch(error) {
+				console.log(error);
+			}
+			Users.addPoints(messageReaction.message.author.id, -1);
+		}
+	},
+};
