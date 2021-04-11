1dafc1c9330a130f4195d43b3a8250ad59bcd0cb
armag3ddon <armag3ddon@web.de>
2021-04-11 14:05:40 +0200

    Removed unused par.

--- commands/coffee.js
+++ commands/coffee.js
@@ -3,7 +3,7 @@ module.exports = {
 	aliases: ['coffee', 'schwarzesgold'],
 	description: 'Ein Tässchen gefällig?',
 	filename: 'coffee',
-	execute(message, args) {
+	execute(message) {
 		const coffee = ['Americano', 'Filterkaffee', 'Espresso', 'Cappucino', 'Latte Macchiato', 'Mokka', 'Cortado', 'Espresso Macchiato', 'Flat White', 'Caffè Latte'];
 		message.channel.send(':coffee:');
 		message.channel.send('Ich trinke jetzt einen ' + coffee[Math.floor(Math.random() * coffee.length)] + '.');
