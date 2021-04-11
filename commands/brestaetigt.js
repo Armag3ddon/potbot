a1a9c3bfd237b60065a0d3446bde5ce9208a7730
armag3ddon <armag3ddon@web.de>
2021-04-11 14:05:56 +0200

    Add ber command.

---
+++ commands/brestaetigt.js
@@ -0,0 +1,9 @@
+module.exports = {
+	name: 'brestätigt',
+	aliases: ['brestätigung', 'brestaetigung'],
+	description: 'Bregor kann das nur brestätigen!',
+	filename: 'brestaetigt',
+	execute(message) {
+		message.channel.send('https://i.imgur.com/TQOFUmQ.mp4');
+	},
+};
