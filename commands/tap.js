a33a4f194ec48fd8ce42aebf1a45f099cdb3efc8
armag3ddon <armag3ddon@web.de>
2021-04-11 14:35:50 +0200

    Add tap command.

---
+++ commands/tap.js
@@ -0,0 +1,10 @@
+module.exports = {
+	name: 'aufschärfen',
+	aliases: ['zapfen', 'tap', 'gelage'],
+	description: 'Eine Runde für alle.',
+	filename: 'tap',
+	execute(message) {
+		message.channel.send(':beer::beer::beer::beer::beer::beer::beer::beer::beer::beer:');
+		message.channel.send('https://i.imgur.com/Z6hWgkx.mp4');
+	},
+};
