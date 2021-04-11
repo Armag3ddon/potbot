1db43db851a5f2f0d14df5c6f0f929ab3f8101a8
armag3ddon <armag3ddon@web.de>
2021-04-11 14:05:23 +0200

    Tweak beer command.

--- commands/beer.js
+++ commands/beer.js
@@ -3,7 +3,9 @@ module.exports = {
 	aliases: ['beer', 'äppler', 'saufn'],
 	description: 'Saufn, eh!',
 	filename: 'beer',
-	execute(message, args) {
+	execute(message) {
+		const beers = [' Pils', ' Alt', ' Helles', ' Weizen', ' Kristallweizen', ' Radler', ' Bockbier', ' IPA', ' Pale Ale', ' Stout', ' Hefeweizen', ' Saskatoon Cheesecake Stout', ' Dubbel', ' Tripel', ' Quadrupel', ' Lager', ' Porter', ' Red Ale', ' Chocolate Stout', ' Lambic', ' West Coast IPA', 'en Maibock', ' Oude Schaarbeekse Kriek', ' Imperial Stout', 'e Berliner Weiße'];
 		message.channel.send(':beers:');
+		message.channel.send('Ich trinke jetzt ein' + beers[Math.floor(Math.random() * beers.length)] + '.');
 	},
 };
