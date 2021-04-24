7bb08bfb9fa6c6d52ca600be73de585a49db9ee8
armag3ddon <armag3ddon@web.de>
2021-04-24 11:55:23 +0200

    Fix !wine for custom drinks.

--- commands/wine.js
+++ commands/wine.js
@@ -4,13 +4,13 @@ module.exports = {
 	description: 'Gehoben trinken.\n!wein oder !wein Sorte.',
 	filename: 'wine',
 	execute(message, args) {
-		const beers = ['Chardonnay', 'Gewürztraminer', 'Müller-Thurgau', 'Muskateller', 'Riesling', 'Sauvignon Blanc', 'Cabernet Sauvignon', 'Grenache', 'Merlot', 'Nebbiolo', 'Pinot Noir', 'Sangiovese', 'Syrah', 'Tempranillo', 'Zinfandel', 'Rotwein', 'Weißwein', 'Rosé'];
+		const wines = ['Chardonnay', 'Gewürztraminer', 'Müller-Thurgau', 'Muskateller', 'Riesling', 'Sauvignon Blanc', 'Cabernet Sauvignon', 'Grenache', 'Merlot', 'Nebbiolo', 'Pinot Noir', 'Sangiovese', 'Syrah', 'Tempranillo', 'Zinfandel', 'Rotwein', 'Weißwein', 'Rosé'];
 		let selection;
 		if (args.length > 0) {
-			selection = ' ' + args.join(' ');
+			selection = args.join(' ');
 		}
 		else {
-			selection = beers[Math.floor(Math.random() * beers.length)];
+			selection = wines[Math.floor(Math.random() * wines.length)];
 		}
 		message.channel.send(':wine_glass:');
 		message.channel.send('Ich trinke jetzt einen ' + selection + '.');
