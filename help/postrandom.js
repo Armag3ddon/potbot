78bf3634d29552766d65527734c89b1d491841ad
armag3ddon <armag3ddon@web.de>
2021-04-28 21:54:07 +0200

    Change from message to embed.

--- help/postrandom.js
+++ help/postrandom.js
@@ -1,16 +1,20 @@
 /* Send a random string from an array of string to the channel, possibly reacting to the message */
 
+const { MessageAttachment } = require('discord.js');
+
 module.exports = {
 	execute(message, set, reaction) {
 		if (set.length == 0) return undefined;
 
 		const rnd = set[Math.floor(Math.random() * set.length)];
+		const attachment = new MessageAttachment(rnd);
 
+		console.log(attachment);
 		if (reaction) {
-			message.channel.send(rnd).then(msg => msg.react(reaction));
+			message.channel.send(attachment).then(msg => msg.react(reaction));
 		}
 		else {
-			message.channel.send(rnd);
+			message.channel.send(attachment);
 		}
 		return rnd;
 	},
