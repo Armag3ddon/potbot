b85815943395d1bd998e6c951f2c330f26572ed1
armag3ddon <armag3ddon@web.de>
2021-04-28 22:18:56 +0200

    Make addimg a hidden command.

--- commands/addimg.js
+++ commands/addimg.js
@@ -3,6 +3,7 @@ const randomimgcommand = require('../help/randomimgcommand.js');
 module.exports = {
 	name: 'addimg',
 	filename: 'addimg',
+	hidden: true,
 	async execute(message, args) {
 		const command = args.shift();
 		console.dir(args);
